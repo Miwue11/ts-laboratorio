5470a286696bf0d78525a5b2d20e49c2d09a34d5
Miguel Lopez Rodriguez <hr2miguellopez@gmail.com>
2025-02-19 15:04:39 +0100

    lab-2

---
+++ src/lab-1.ts
@@ -0,0 +1,13 @@
+
+const ticket= 120
+
+const bebidas=18
+
+const comida= ticket - bebidas
+
+console.log(`El costo de la comida es de ${comida} euros`)
+
+const pago_po_comensal= comida / 6
+
+console.log(`El pago por comensal es de ${pago_po_comensal} euros`)
+
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,78 @@
 
-const ticket= 120
+interface GrupoMusical{
+    nombreGrupo: string;
+    cantantes: string[];
+    compositor: string;
+    año: number;
+    activo: boolean;
+    genero: string;
 
-const bebidas=18
+}
 
-const comida= ticket - bebidas
+const rock= "🎸 Rock";
+const pop= "🎵 Pop Rock";
+const hardRock= "🤘 Hard Rock";
+const clasica= "🎼 Clasica";
 
-console.log(`El costo de la comida es de ${comida} euros`)
+const color="background-color:green;font-weight:bold;font-size:20px";
 
-const pago_po_comensal= comida / 6
 
-console.log(`El pago por comensal es de ${pago_po_comensal} euros`)
+let grupo1: GrupoMusical = {
+    nombreGrupo: "The Beatles",
+    cantantes: ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"],
+    compositor: "John Lennon",
+    año: 1960,
+    activo: true,
+    genero: `- ${pop}`
 
+}
+
+let grupo2: GrupoMusical = {
+    nombreGrupo: 'Queen',
+    cantantes: ['Freddie Mercury', 'Brian May', 'Roger Taylor', 'John Deacon'],
+    compositor: 'Freddie Mercury',
+    año: 1970,
+    activo: false,
+    genero: `- ${rock}`
+
+
+}
+
+let grupo3: GrupoMusical = {
+    nombreGrupo: 'AC DC',
+    cantantes: ['Bon Scott', 'Brian Johnson'],
+    compositor: 'Angus Young',
+    año: 1973,
+    activo: true,
+    genero: `- ${hardRock}`
+
+}
+
+let grupo4: GrupoMusical = {
+    nombreGrupo: 'Ludwing Van Beethoven',
+    cantantes: ['Ludwing Van Beethoven'],
+    compositor: 'Ludwing Van Beethoven',
+    año: 1770,
+    activo: false,
+    genero: `- ${clasica}`
+
+}
+
+let grupo5: GrupoMusical = {
+    nombreGrupo: '%cThe Rolling Stones',
+    cantantes: ['Mick Jagger', 'Keith Richards', 'Charlie Watts', 'Ronnie Wood'],
+    compositor: 'Mick Jagger',
+    año: 1962,
+    activo: true,
+    genero: `- ${rock}`
+}
+console.log(`%cThe Beatles`,color);
+console.log(grupo1,);
+console.log(`%cQueen`,color);
+console.log(grupo2);
+console.log(`%cAC DC`,color);
+console.log(grupo3);
+console.log(`%cLudwing Van Beethoven`,color);
+console.log(grupo4);
+console.log(`%cThe Rolling Stones`,color);
+console.log(grupo5);

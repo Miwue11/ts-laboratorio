c7b43cd997db161db4579554d8850a2105d3e7d1
Miguel Lopez Rodriguez <hr2miguellopez@gmail.com>
2025-02-24 09:34:20 +0100

    Lab-2-actualizado.ts

--- src/lab-1.ts
+++ src/lab-1.ts
@@ -1,13 +1,28 @@
+interface GrupoMusical {
+    nombreGrupo: string;
+    cantantes: string[];
+    compositor: string;
+    año: number;
+    activo: boolean;
+    genero: string;
+}
 
-const ticket= 120
+const rock = "🎸 Rock";
+const pop = "🎵 Pop Rock";
+const hardRock = "🤘 Hard Rock";
+const clasica = "🎼 Clasica";
+const color = "background-color:green;font-weight:bold;font-size:20px";
 
-const bebidas=18
+function crearGrupoMusical(nombreGrupo: string, cantantes: string[], compositor: string, año: number, activo: boolean, genero: string): GrupoMusical {
+    return { nombreGrupo, cantantes, compositor, año, activo, genero };
+}
 
-const comida= ticket - bebidas
+const gruposMusicales: GrupoMusical[] = [
+    crearGrupoMusical("The Beatles", ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"], "John Lennon", 1960, true, `- ${pop}`),
+    crearGrupoMusical("Queen", ["Freddie Mercury", "Brian May", "Roger Taylor", "John Deacon"], "Freddie Mercury", 1970, false, `- ${rock}`),
+    crearGrupoMusical("AC/DC", ["Bon Scott", "Brian Johnson"], "Angus Young", 1973, true, `- ${hardRock}`),
+    crearGrupoMusical("Ludwig Van Beethoven", ["Ludwig Van Beethoven"], "Ludwig Van Beethoven", 1770, false, `- ${clasica}`),
+    crearGrupoMusical("The Rolling Stones", ["Mick Jagger", "Keith Richards", "Charlie Watts", "Ronnie Wood"], "Mick Jagger", 1962, true, `- ${rock}`)
+];
 
-console.log(`El costo de la comida es de ${comida} euros`)
-
-const pago_po_comensal= comida / 6
-
-console.log(`El pago por comensal es de ${pago_po_comensal} euros`)
-
+gruposMusicales.forEach(grupo => console.log(`%c${grupo.nombreGrupo}`, color, grupo));
